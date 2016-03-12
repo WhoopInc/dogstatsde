5975f2769ff0d589be50e78164a8bdc500b04eb3
Nathaniel Waisbrot <waisbrot@whoop.com>
2016-03-11 22:12:42 -0500

    travis try 2

--- scripts/install-rebar.sh
+++ scripts/install-rebar.sh
@@ -2,16 +2,18 @@
 
 mkdir vendor
 if [ $REBAR_VSN -eq 2 ]; then
-    if [ x$(which rebar)x = xx ]; then
+    if [ x$(which rebar)x != xx ]; then
         ln -s $(which rebar) vendor/rebar
     else
         curl -L https://github.com/rebar/rebar/wiki/rebar -o vendor/rebar
+        chmod a+x vendor/rebar
     fi
 elif [ $REBAR_VSN -eq 3 ]; then
-    if [ x$(which rebar3)x = xx ]; then
+    if [ x$(which rebar3)x != xx ]; then
         ln -s $(which rebar3) vendor/rebar3
     else
         curl -L https://s3.amazonaws.com/rebar3/rebar3 -o vendor/rebar3
+        chmod a+x vendor/rebar3
     fi
 else
     echo Unknown rebar version requested: $REBAR_VSN
